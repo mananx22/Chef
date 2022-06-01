c48ca608eedd8914c1dea109caea7884e46cbe66
mananx22 <motorollaman27@gmail.com>
2022-06-01 05:55:50 -0400

    3 added only if statement

--- 1 Basic.rb
+++ 1 Basic.rb
@@ -1,14 +1,23 @@
 package 'apache' do
-    package_name 'httpd'
+  package_name 'httpd'
 end
 
 service 'httpd' do
-    action [:enable, :start]
+  action [:enable, :start]
 end
 
 file '/var/www/index.html' do
-    content 'Hello Manan'
-    mode '0755'
-    owner 'root'
-    group 'apache'
+  content 'Hello Manan'
+  mode '0755'
+  owner 'root'
+  group 'apache'
+end
+
+file '/etc/manan.txt' do
+    content ' He says .... '
+end
+
+execute 'commander or anything' do
+    command 'echo blah >> /etc/manan.txt'
+    only_if 'test -r /etc/manan.txt'
 end
